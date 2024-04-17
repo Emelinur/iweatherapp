128393c5e4f34fec39a518e5461b7b79417d8a8a
Emelinur <emelinuryzgc@gmail.com>
2024-04-17 16:32:25 +0300

    git commit -m "uygulama eklendi"

---
+++ src/App.jsx
@@ -0,0 +1,60 @@
+import axios from "axios";
+import React, { useState } from "react";
+import WeatherApp from "./Componets/WeatherApp";
+import Header from "./Componets/Header";
+import Cities from "./Componets/Cities";
+import './App.css'
+
+
+function App() {
+  const [citySearch, setCitySearch] = useState("");
+  const [cityData, setCityData] = useState(null);
+  const [error, setError] = useState(null);
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await  axios.get(
+        `http://dataservice.accuweather.com/locations/v1/cities/search?apikey=%09${import.meta.env.VITE_API_KEY}&q=${citySearch}`
+      );
+      if (response.data && response.data.length > 0) {
+        setCityData(response.data[0]);
+        setError(null);
+      } else {
+        setError("City not found");
+        setCityData(null);
+      }
+    } catch (error) {
+      console.error("Error fetching city data:", error);
+      setError("An error occurred. Please try again later.");
+      setCityData(null);
+    }
+  };
+
+  return (
+    <div className="container">
+      <Header />
+      <div className="row">
+        <div className="search_header">
+          <h3>
+            Welcome to <span>TypeWeather</span>
+          </h3>
+          <p>Choose a location to see the weather forecast</p>
+        </div>
+        <form className="searchManifier" onSubmit={onSubmit}>
+          <input
+            placeholder="Enter city name"
+            className="search"
+            value={citySearch}
+            onChange={(e) => setCitySearch(e.target.value)}
+          />
+        </form>
+        {error && <p className="error">{error}</p>}
+        {cityData && <WeatherApp cityData={cityData} />}
+        <Cities />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Componets/WeatherApp.jsx
@@ -0,0 +1,84 @@
+import axios from "axios";
+import React, { useState, useEffect } from "react";
+
+import Clear from "./img/Clear.png";
+import ClearBg from "./img/ClearBg.png";
+import ClearNight from "./img/ClearNight.png";
+import ClearNightBg from "./img/ClearNightBg.png";
+
+
+import Cloudy from "./img/Cloudy.png";
+import CloudyNight from "./img/CloudyNight.png";
+
+import PartlySunny from "./img/PartlySunny.png";
+import PartlySunnyNight from "./img/PartlySunnyNight.png";
+
+import Rain from "./img/Rain.png";
+import RainNight from "./img/RainNight.png";
+
+import Thunderstorm from "./img/Thunderstorm.png";
+import ThunderstormNight from "./img/ThunderstormNight.png";
+
+function WeatherApp({ cityData }) {
+  const [data, setData] = useState("");
+  useEffect(() => {
+    const fetchWeather = async () => {
+      try {
+        const response = await axios.get(`http://dataservice.accuweather.com/currentconditions/v1/${cityData.Key}?apikey=${import.meta.env.VITE_API_KEY}`);
+        setData(response.data[0]);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchWeather();
+  }, [cityData]);
+
+  const renderWeatherIcon = () => {
+    if (!data) return null;
+
+    const { IsDayTime, WeatherIcon } = data;
+
+    // Gündüz ve gece için uygun görseli seç
+    const dayOrNight = IsDayTime ? "Day" : "Night";
+
+    // WeatherIcon'a göre uygun görseli seç
+    switch (WeatherIcon) {
+      case 1:
+        return IsDayTime ?
+          <div>
+            <img className="images" src={Clear} alt="Clear" />
+           
+          </div>
+          :
+          <div>
+            <img className="images" src={ClearNight} alt="ClearNight" />;
+
+          </div>
+      case 6:
+      case 7:
+      case 38:
+        return IsDayTime ? <img className="images" src={Cloudy} alt="Cloudy" /> : <img className="images" src={CloudyNight} alt="CloudyNight" />;
+      case 3:
+        return IsDayTime ? <img className="images" src={PartlySunny} alt="PartlySunny" /> : <img className="images" src={PartlySunnyNight} alt="PartlySunnyNight" />;
+      case 12:
+        return IsDayTime ? <img className="images" src={Rain} alt="Rain" /> : <img className="images" src={RainNight} alt="RainNight" />;
+      case 15:
+        return IsDayTime ? <img className="images" src={Thunderstorm} alt="Thunderstorm" /> : <img className="images" src={ThunderstormNight} alt="ThunderstormNight" />;
+      default:
+        return null; // İkon tanımlı değilse null döndür
+    }
+  };
+
+  return (
+    <div className="scard">
+      <h2>{cityData.EnglishName}</h2>
+      <div className="row1">
+        <div className="col-left">{data ? Math.ceil(data.Temperature.Metric.Value) : null}°C</div>
+        <div className="col-right">{renderWeatherIcon()}</div>
+      </div>
+    </div>
+  );
+}
+
+export default WeatherApp;
+
